f8e84ae66029313fa8c9ce09f71265953bd6dfe2
LiamF7 <flinntwins09@gmail.com>
2026-02-07 15:43:09 -0700

    2/7/2026

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -12,7 +12,7 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.units.measure.Distance;
-import frc.robot.vision.VisionCamera;
+//import frc.robot.vision.VisionCamera;
 
 public class VisionConstants {
   public static final Distance CULLING_DISTANCE = Meters.of(4); // Meters.of(2.5);
@@ -22,28 +22,6 @@ public class VisionConstants {
   private static final String FRONT_LEFT_CAMERA_NAME = "OV9281FrontLeft";
   private static final String FRONT_RIGHT_CAMERA_NAME = "OV9281FrontRight";
   private static final String REAR_CAMERA_NAME = "OV9281Rear";
-
-  public static final Transform3d ROBOT_TO_FRONT_LEFT_CAMERA =
-      new Transform3d(
-          new Translation3d(-.272, 0.172, 0.711),
-          new Rotation3d(Radians.zero(), Degrees.of(0), Degrees.zero()));
-
-  private static final Transform3d ROBOT_TO_FRONT_RIGHT_CAMERA =
-      new Transform3d(
-          new Translation3d(.1524, -0.254, 0.4617),
-          new Rotation3d(Radians.zero(), Degrees.of(22), Degrees.zero()));
-
-  private static final Transform3d ROBOT_TO_REAR_CAMERA =
-      new Transform3d(
-          new Translation3d(-.019, 0.0, 0.957),
-          new Rotation3d(Radians.zero(), Degrees.of(-27), Degrees.of(180)));
-
-  public static final VisionCamera FRONT_LEFT_CAMERA =
-      new VisionCamera(FRONT_LEFT_CAMERA_NAME, ROBOT_TO_FRONT_LEFT_CAMERA);
-  public static final VisionCamera FRONT_RIGHT_CAMERA =
-      new VisionCamera(FRONT_RIGHT_CAMERA_NAME, ROBOT_TO_FRONT_RIGHT_CAMERA);
-  public static final VisionCamera REAR_CAMERA =
-      new VisionCamera(REAR_CAMERA_NAME, ROBOT_TO_REAR_CAMERA);
 
   // The standard deviations of our vision estimated poses, which affect correction rate
   public static final Matrix<N3, N1> SINGLE_TAG_STD_DEVS = VecBuilder.fill(0.001, 0.001, 0.001);
