7bfeb052bcbe0e52d41fcd6aefe7d8b1bb1032c4
Ben Griffin <Bennett.Griffin@gmail.com>
2026-03-02 21:06:42 -0700

    Three meter measure

--- src/main/java/frc/robot/commands/InterpolatedShootCommand.java
+++ src/main/java/frc/robot/commands/InterpolatedShootCommand.java
@@ -28,12 +28,14 @@ public class InterpolatedShootCommand {
     flywheelSpeedMap.put(3.5, 2700.0);
     flywheelSpeedMap.put(5.2, 3150.0);
     flywheelSpeedMap.put(4.0, 2800.0);
+    flywheelSpeedMap.put(3.0, 2650.0);
 
     hoodAngleMapMap.put(2.55, 0.2);
     hoodAngleMapMap.put(1.8, 0.1);
     hoodAngleMapMap.put(3.5, 0.4);
     hoodAngleMapMap.put(5.2, 0.6);
     hoodAngleMapMap.put(4.0, 0.5);
+    hoodAngleMapMap.put(3.0, 0.25);
   }
 
   public InterpolatedShootCommand(
