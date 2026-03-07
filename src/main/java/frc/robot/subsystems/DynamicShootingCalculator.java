f5d003b57aed596427952dfa4ce86d36eec7d870
Ben Griffin <Bennett.Griffin@gmail.com>
2026-03-07 11:56:35 -0700

    Revert "After Q12"
    This reverts commit 2cf811b1463c10f928da9d9ff00d21c8bf349bca.

--- src/main/java/frc/robot/subsystems/DynamicShootingCalculator.java
+++ src/main/java/frc/robot/subsystems/DynamicShootingCalculator.java
@@ -131,17 +131,16 @@ public class DynamicShootingCalculator {
 
     double lookaheadTurretToTargetDistance = turretToTarget;
 
-    // Iterate for moving
-    // for (int i = 0; i < 3; i++) {
-    //   timeOfFlight = timeOfFlightMap.get(lookaheadTurretToTargetDistance);
-    //   double offsetX = turretVelocityX * timeOfFlight / 4.0;
-    //   double offsetY = turretVelocityY * timeOfFlight / 4.0;
-    //   lookaheadPose =
-    //       new Pose2d(
-    //           turretPose.getTranslation().plus(new Translation2d(offsetX, offsetY)),
-    //           turretPose.getRotation());
-    //   lookaheadTurretToTargetDistance = target.getDistance(lookaheadPose.getTranslation());
-    // }
+    for (int i = 0; i < 1; i++) {
+      timeOfFlight = timeOfFlightMap.get(lookaheadTurretToTargetDistance);
+      double offsetX = turretVelocityX * timeOfFlight / 4.0;
+      double offsetY = turretVelocityY * timeOfFlight / 4.0;
+      lookaheadPose =
+          new Pose2d(
+              turretPose.getTranslation().plus(new Translation2d(offsetX, offsetY)),
+              turretPose.getRotation());
+      lookaheadTurretToTargetDistance = target.getDistance(lookaheadPose.getTranslation());
+    }
 
     // Calculate parameters accounted for imparted velocity
     turretAngle = target.minus(lookaheadPose.getTranslation()).getAngle();
