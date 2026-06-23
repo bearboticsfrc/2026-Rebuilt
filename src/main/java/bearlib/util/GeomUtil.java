e4a53b4f720d802b10410290ed5f45a9f113b22d
LiamF7 <flinntwins09@gmail.com>
2026-06-23 15:37:03 -0600

    Update GeomUtil
    6/23/2026

--- src/main/java/bearlib/util/GeomUtil.java
+++ src/main/java/bearlib/util/GeomUtil.java
@@ -28,20 +28,20 @@ public class GeomUtil {
     double dist = Double.MAX_VALUE;
 
     for (int n = zone.coords.length, i = 0; i < n; i++) {
-      // horizontal displacement of border seg
+      // horizontal displacement of zone seg
       double xSeg = zone.coords[(i + 1) % n].getX() - zone.coords[i].getX();
-      // vertical displacement of border seg
+      // vertical displacement of zone seg
       double ySeg = zone.coords[(i + 1) % n].getY() - zone.coords[i].getY();
-      // horizontal displacement of robot to border seg
+      // horizontal displacement of robot to zone seg
       double xBot = pose.getX() - zone.coords[i].getX();
-      // vertical displacement of robot to border seg
+      // vertical displacement of robot to zone seg
       double yBot = pose.getY() - zone.coords[i].getY();
 
-      // % of closest coord along border seg
+      // % of closest coord along zone seg
       double t =
           MathUtil.clamp(((xBot * xSeg) + (yBot * ySeg)) / ((xSeg * xSeg) + (ySeg * ySeg)), 0, 1);
 
-      // closest coord along border seg
+      // closest coord along zone seg
       Translation2d c =
           new Translation2d(zone.coords[i].getX() + (t * xSeg), zone.coords[i].getY() + (t * ySeg));
 
@@ -67,7 +67,7 @@ public class GeomUtil {
       double y1 = zone.coords[i].getY();
       double x1 = zone.coords[i].getX();
       double y2 = zone.coords[(i + 1) % n].getY();
-      double x2 = zone.coords[(i + 1) % n].getY();
+      double x2 = zone.coords[(i + 1) % n].getX();
 
       if ((pose.getY() < y1) != (pose.getY() < y2)) {
         double px = (x2 - x1) * (pose.getY() - y1) / (y2 - y1) + x1;
